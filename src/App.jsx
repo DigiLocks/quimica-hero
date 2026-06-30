09997bd2b97df6a28d971572e96bb2d52cc3bbdf
DigiLocks <catalinutzu5@yahoo.com>
2026-06-30 21:14:55 +0000

    feat: live chat bubble with quick replies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import ChatBubble from "./components/ChatBubble.jsx";
 import MouseGradient from "./components/MouseGradient.jsx";
 export default function App() {
   return (
     <div className="relative min-h-screen w-full overflow-hidden bg-black">
       <MouseGradient />
+      <ChatBubble />
       <Hero />
     </div>
   );
